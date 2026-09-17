8d785286a654e05e78aaddc45a2461d3c20c6054
RJ-Rishi91 <rushalofficial@gmail.com>
2026-09-17 06:00:28 +0530

    chore(cors): support dynamic origin regex for production frontend deployments

--- backend/server.py
+++ backend/server.py
@@ -1471,6 +1471,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_origins=cors_origins,
+    allow_origin_regex=r"^https?:\/\/.*",
     allow_methods=["*"],
     allow_headers=["*"],
 )
